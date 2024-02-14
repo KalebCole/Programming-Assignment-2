a830675bc7b6a50ebb3e5f779fc4b73f1916de30
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-02-14 12:08:52 -0500

    completed all the main submission stuff (besides bootstrap), working on extra credit

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
+/*
+Main logic for the program submission
+*/
 window.addEventListener("load", () => {
     document.getElementById("errorMessageDiv").style.visibility = "hidden";
+    // document.getElementById("navigationBar").style.visibility = "hidden";
+});
+
+
+// this will make the error message visible if not valid
+document.getElementById("submitButton").addEventListener("click", () => {   
+    if(validate()){
+        console.log("Valid input");
+        document.getElementById("errorMessageDiv").style.visibility = "hidden";
+        document.querySelector("#dataEntryDiv form").submit();
+    }
+    else{
+        console.log("Invalid input");
+        document.querySelector("#errorMessageDiv #errorMessage").innerHTML = "Invalid input";
+        document.getElementById("errorMessageDiv").style.visibility = "visible";
+    }
 });
 
 
 
-
 function validate() {
-    var nameInput = document.getElementById("nameDiv").value;
-    var ageInput = document.getElementById("ageDiv").value;
-    if (isNaN(nameInput) || ageInput < 1 || ageInput > 10) {
+    var nameInput = document.querySelector("#nameInput").value;
+    var ageInput = document.querySelector("#ageInput").value; 
+    if (ageInput < 1 || ageInput > 100 || isNaN(ageInput) || nameInput == "" || nameInput == null || nameInput == undefined) {
         return false;
     }
     return true;
 
 }
+
+
+/*
+Extra Credit Codes
+*/
+
+// this will make the unordered list visible if the user clicks the load list button
+// document.getElementById("loadingButtonDiv").addEventListener("click", () => {
+//     document.getElementById("loadingButtonDiv").style.visibility = "hidden";
+//     document.querySelector(".tenor-gif-embed").style.visibility = "hidden";
+//     document.getElementById("navigationBar").style.visibility = "visible";
+// });
